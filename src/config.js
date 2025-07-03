f7efce07db96fd5018da49daaabba3c6c500de9b
Nick Shurtleff <nshurtleff15@gmail.com>
2025-07-02 20:18:20 -0400

    First real deploy, switching bot over to fantasy nascar group, let's see how it goes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 exports.ACCESS_TOKEN = "AH1OT9rf0u1JOH6WX8XrQ68u3tRZOh0UOm4eQ660";
-exports.BOT_ID = "a651cda2d1ecdb3959608b8a01";
-exports.GROUP_ID = "48392578";
+exports.BOT_ID = "2e41c179b8182bc536007ee2f2";
+exports.GROUP_ID = "108480647";
 exports.GIHPY_KEY = "3cNm0IxmL2htInLe6AUQBfUQ7tbYU0IC";
 exports.ACCESS_TOKEN_2 = "4LffyUZhNDfb3iGQAea0Mu1EvhYaFajTH5PYdqLj";
 exports.DATABASE_HOST = '195.35.53.21';
